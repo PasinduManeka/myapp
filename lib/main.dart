37df37155ef7aa6eaafe8cce2a215d4187656dfa
manekaherat815@gmail.com <manekaherat815@gmail.com>
2022-03-07 09:09:44 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hello my friend'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text('click'),
+      ),
+    )
+  ));
+}
+

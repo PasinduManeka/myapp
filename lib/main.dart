7a99b854d62d833783b26d66329f882b9766b76b
manekaherat815@gmail.com <manekaherat815@gmail.com>
2022-03-16 08:26:06 +0530

    Added the network image

--- lib/main.dart
+++ lib/main.dart
@@ -21,15 +21,8 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[600],
       ),
       body: Center(
-        child: Text(
-          'Hello Mates!',
-          style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower',
-          ),
+        child: Image(
+          image:NetworkImage('https://images.unsplash.com/photo-1572973687274-74a2b3db827c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=454&q=80'),
         ),
       ),
       floatingActionButton: FloatingActionButton(

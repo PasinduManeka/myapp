463eb0db15e86eea532b7713a852c6959a4bf20d
manekaherat815@gmail.com <manekaherat815@gmail.com>
2022-04-09 15:47:53 +0530

    Add a button.

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,11 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.green[600],
       ),
       body: Center(
-        child: Image.asset('assets/space2.jpg'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color:Colors.lightBlue,
+          size: 50.0,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {  },
